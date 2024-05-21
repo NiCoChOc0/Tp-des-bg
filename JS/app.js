15b7bb7faf17942e5f9bea8c1077431d6ce650ea
NiCoChOc0 <nicolasbouydron.cd@gmail.com>
2024-05-21 08:19:21 +0200

    coucou2

--- JS/app.js
+++ JS/app.js
@@ -29,3 +29,47 @@ function render() {
     <div class="square"></div>
     `;
 }
+
+// EX 1
+function changeColor(event){
+    let colors = ["yellow", "blue", "red"];
+
+    color_index++;
+    if (color_index >= colors.length){
+        color_index=0;
+    }
+    color_btn.textContent = colors[color_index].charAt(0).toUpperCase() + colors[color_index].slice(1);
+    color_btn.className = colors[color_index];
+}
+let color_index = 0;
+let color_btn = document.getElementById("color_btn");
+color_btn.addEventListener('click', changeColor);
+
+// EX 2
+let iterations_count = document.getElementById('iterations_count');
+let iterations_slider = document.getElementById('iterations_slider');
+iterations_slider.addEventListener("input", e=>iterations_count.textContent = e.target.value);
+
+// EX 3
+let run_btn = document.getElementById("run_btn");
+
+run_btn.addEventListener('click', e=>{
+    for (i=0; i<=iterations_slider.value; i++){
+        console.log(i);
+    }
+});
+
+// EX 4
+let circle = document.getElementsByClassName("circle")[0];
+let square = document.getElementsByClassName("square")[0];
+function moveItem(item){
+    let new_margin_value = +(item.style.marginLeft.slice(0, -2)) + 1;
+
+    if (new_margin_value + item.style.width >= document.body.clientWidth){
+        new_margin_value = 0;
+    }
+
+    item.style.marginLeft = new_margin_value + "px";
+}
+setInterval(moveItem, 10, circle);
+setInterval(moveItem, 10, square);
